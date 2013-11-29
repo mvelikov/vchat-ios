afcb981a3f71d23f2569e73519eaaf7c2015ed2f
Mihail Velikov <mihailvelik@gmail.com>
2013-11-29 22:25:20 +0200

    connecting to PubNub on app load

--- VChat/VCAppDelegate.m
+++ VChat/VCAppDelegate.m
@@ -20,6 +20,35 @@
 //    [VCUser init];
     [PubNub setDelegate:self];
     // Override point for customization after application launch.
+    
+    [PubNub setConfiguration:[PNConfiguration configurationForOrigin:kOrigin publishKey:kPubKey subscribeKey:kSubKey secretKey:kSecret]];
+    
+    [PubNub connectWithSuccessBlock:^(NSString *origin) {
+        PNLog(PNLogGeneralLevel, self, @"{BLOCK} PubNub client connected to: %@", origin);
+        
+        int64_t delayInSeconds = 1.0;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [PubNub subscribeOnChannel:[PNChannel channelWithName:@"channel1" shouldObservePresence:YES]]; });
+        
+    } errorBlock:^(PNError *connectionError) {
+        
+        if (connectionError.code == kPNClientConnectionFailedOnInternetFailureError) {
+            // wait 1 second
+            int64_t delayInSeconds = 1.0;
+            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+            dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+                PNLog(PNLogGeneralLevel, self, @"Connection will be established as soon as internet connection will be restored");
+            });
+        }
+        UIAlertView *connectionErrorAlert = [UIAlertView new]; connectionErrorAlert.title = [NSString stringWithFormat:@"%@(%@)",
+                                                                                             [connectionError localizedDescription],
+                                                                                             NSStringFromClass([self class])];
+        connectionErrorAlert.message = [NSString stringWithFormat:@"Reason:\n%@\n\nSuggestion:\n%@",
+                                        [connectionError localizedFailureReason],
+                                        [connectionError localizedRecoverySuggestion]]; [connectionErrorAlert addButtonWithTitle:@"OK"];
+        [connectionErrorAlert show];
+    }];
     return YES;
 }
 							
