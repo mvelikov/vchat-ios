180347f1c70a09871550adeb6eb3b426a91162c7
Mihail Velikov <mihailvelik@gmail.com>
2013-11-30 17:47:34 +0200

    change channel property type in VCMessage to VCChannel

--- VChat/VCMessage.h
+++ VChat/VCMessage.h
@@ -7,15 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "VCChannel.h"
 @interface VCMessage : NSObject {
     @private NSString* message;
-    @private NSString* channel;
+    @private VCChannel* channel;
     @private VCUser* user;
 }
 
 @property (nonatomic, retain) NSString* message;
-@property (nonatomic, retain) NSString* channel;
+@property (nonatomic, retain) VCChannel* channel;
 @property (nonatomic, retain) VCUser* user;
 
 @end
